1f0560d85c2de8167fa4bfe6a5c2e49a72b885ab
makskhv21 <khvits21@gmail.com>
2024-03-15 18:10:01 +0200

    Completed implementation_test.go

--- implementation_test.go
+++ implementation_test.go
@@ -7,17 +7,64 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
+func TestPrefixToPostfix1(t *testing.T) {
+	res, err := PrefixToPostfix("+ 6 8") 
 	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
+		assert.Equal(t, "6 8 +", res)
 	}
 }
 
+func TestPrefixToPostfix2(t *testing.T) {
+	res, err := PrefixToPostfix("* - 3 4 2")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "3 4 - 2 *", res)
+	}
+}
+
+func TestPrefixToPostfix3(t *testing.T) {
+	res, err := PrefixToPostfix("/ * + 1 4 + - 1 4 6 - 2 3")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "1 4 + 1 4 - 6 + * 2 3 - /", res)
+	}
+}
+
+func TestPrefixToPostfix4(t *testing.T) {
+	res, err := PrefixToPostfix("/ * + - - 2 3 2 7 + + + 2 3 7 3 7")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "2 3 - 2 - 7 + 2 3 + 7 + 3 + * 7 /", res)
+	}
+}
+
+func TestPrefixToPostfix5(t *testing.T) {
+	_, err := PrefixToPostfix("")
+
+	assert.NotNil(t, err)
+	if assert.Error(t, err) {
+		assert.Equal(t, fmt.Errorf("Incorrect prefix expression"), err)
+	}
+}
+func TestPrefixToPostfix6(t *testing.T) {
+	_, err := PrefixToPostfix("Plus three  eight")
+
+	assert.NotNil(t, err)
+	if assert.Error(t, err) {
+		assert.Equal(t, fmt.Errorf("Incorrect prefix expression"), err)
+	}
+}
+func TestPrefixToPostfix7(t *testing.T) {
+	_, err := PrefixToPostfix("-....- ..... ...--")
+
+	assert.NotNil(t, err)
+	if assert.Error(t, err) {
+		assert.Equal(t, fmt.Errorf("Incorrect prefix expression"), err)
+	}
+}
+
+// Example demonstrates how to use PrefixToPostfix function.
 func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
+	res, _ := PrefixToPostfix("- 5 8")
 	fmt.Println(res)
 
 	// Output:
-	// 2 2 +
+	// 5 8 -
 }
