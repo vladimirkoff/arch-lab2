619ecb422c48090c159d6472d14bb62523e4893e
Vladimir Kovalev <vovakovalev2016@icloud.com>
2024-03-15 19:43:44 +0300

    refactor main

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 
 	lab2 "github.com/vladimirkoff/arch-lab2"
 )
@@ -18,55 +19,45 @@ var (
 func main() {
 	flag.Parse()
 
-	input, err := getInputReader()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "input failed:", err)
+	var input io.Reader
+
+	if *inputExpression != "" && *inputFile != "" {
+		fmt.Fprintln(os.Stderr, "amount of flags exceeded")
+	}
+
+	if *inputExpression != "" {
+		input = strings.NewReader(*inputExpression)
+	} else if *inputFile != "" {
+		file, err := os.Open(*inputFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "input file failed: "+err.Error())
+		}
+		defer file.Close()
+		input = file
+	} else {
+		fmt.Fprintln(os.Stderr, "input failed")
 		return
 	}
 
-	output, err := getOutputWriter()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "output failed:", err)
-		return
+	var res io.Writer
+	if *outputFile != "" {
+		file, err := os.Create(*outputFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "output file error: "+err.Error())
+		}
+		defer file.Close()
+		res = file
+	} else {
+		res = os.Stdout
 	}
 
 	handler := lab2.ComputeHandler{
 		Input:  input,
-		Output: output,
-	}
-	if err := handler.Compute(); err != nil {
-		fmt.Fprintln(os.Stderr, "computation failed:", err)
-	}
-}
-
-func getInputReader() (io.Reader, error) {
-	if *inputExpression != "" && *inputFile != "" {
-		return nil, fmt.Errorf("exceeded amount of input flags")
+		Output: res,
 	}
 
-	if *inputExpression != "" {
-		return strings.NewReader(*inputExpression), nil
+	err := handler.Compute()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "convertation error: "+err.Error())
 	}
-
-	if *inputFile != "" {
-		file, err := os.Open(*inputFile)
-		if err != nil {
-			return nil, fmt.Errorf("input file error: %s", err)
-		}
-		defer file.Close()
-		return file, nil
-	}
-	return nil, fmt.Errorf("no input")
-}
-
-func getOutputWriter() (io.Writer, error) {
-	if *outputFile != "" {
-		file, err := os.Create(*outputFile)
-		if err != nil {
-			return nil, fmt.Errorf("output file error: %s", err)
-		}
-		defer file.Close()
-		return file, nil
-	}
-	return os.Stdout, nil
 }
